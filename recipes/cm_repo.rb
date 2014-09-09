f64eea53b5b6df657e44f00db935438ae5a310f0
Anton Butovich <abutovich@gmail.com>
2014-09-09 12:17:50 +0300

    changes for yum resource > v3.0.0

--- recipes/cm_repo.rb
+++ recipes/cm_repo.rb
@@ -4,17 +4,16 @@
 arch = node['kernel']['machine'] =~ /x86_64/ ? "x86_64" : "i386"
 
 case node[:platform]
-when "redhat", "centos"
-  relnum = node['platform_version'].to_i
-
-  yum_key "RPM-GPG-KEY-cloudera" do
-    url "#{node.cloudera.repository_url}/cm4/redhat/#{relnum}/#{arch}/cm/RPM-GPG-KEY-cloudera"
+  when "redhat", "centos"
+    relnum = node['platform_version'].to_i
+  when "amazon"
+    relnum = "5"
   end
 
-  yum_repository "cloudera-manager" do
-    description "Cloudera Manager"
-    url "#{node.cloudera.repository_url}/cm4/redhat/#{relnum}/#{arch}/cm/#{node.cloudera.manager.version}/"
-    key "RPM-GPG-KEY-cloudera"
-    enabled 1
-  end
+yum_repository "cloudera-manager" do
+  description "Cloudera's Distribution for Cloudera Manager, Version 4"
+  baseurl "#{node[:cloudera][:repository_url]}/cm4/redhat/#{relnum}/#{arch}/cm/4/"
+  gpgkey "#{node[:cloudera][:repository_url]}/cm4/redhat/#{relnum}/#{arch}/cm/RPM-GPG-KEY-cloudera"
+  enabled true
+  action :create
 end
--- recipes/impala_repo.rb
+++ recipes/impala_repo.rb
@@ -6,15 +6,14 @@ arch = node['kernel']['machine'] =~ /x86_64/ ? "x86_64" : "i386"
 case node[:platform]
   when "redhat", "centos"
     relnum = node['platform_version'].to_i
+  when "amazon"
+    relnum = "5"
+  end
 
-    yum_key "RPM-GPG-KEY-impala" do
-      url "http://archive.cloudera.com/impala/redhat/#{relnum}/#{arch}/impala/RPM-GPG-KEY-cloudera"
-    end
-
-    yum_repository "cloudera-impala" do
-      description "Cloudera Impala repository"
-      url "http://archive.cloudera.com/impala/redhat/#{relnum}/#{arch}/impala/#{node.cloudera.impala.version}/"
-      key "RPM-GPG-KEY-impala"
-      enabled 1
-    end
+yum_repository "cloudera-impala" do
+  description "Cloudera Impala repository"
+  baseurl "http://archive.cloudera.com/impala/redhat/#{relnum}/#{arch}/impala/#{node.cloudera.impala.version}/"
+  gpgkey "http://archive.cloudera.com/impala/redhat/#{relnum}/#{arch}/impala/RPM-GPG-KEY-cloudera"
+  enabled true
+  action :create
 end
