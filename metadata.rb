3983669f42441f8fb2d68222e3d26e6c1bb5d6a0
Anton Butovich <abutovich@gmail.com>
2014-09-09 12:19:24 +0300

    Added metadata for berks support

--- metadata.rb
+++ metadata.rb
@@ -1,1 +1,14 @@
-depends "yum"
+maintainer       "Qubell Inc."
+maintainer_email "abutovich@qubell.com"
+license          "All rights reserved"
+description      "Provision Cloudera Hadoop"
+name		 "cloudera"
+version          "0.0.1"
+
+depends "yum", '~> 3.3.1'
+depends "ntp", '~> 1.6.4'
+depends "java", '=1.26.0'
+depends 'mysql', '=4.0.14'
+depends 'database', '=1.6.0'
+depends 'python', '=1.4.6'
+depends 'selinux', '~> 0.6.0'
