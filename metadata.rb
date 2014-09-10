13c37ff301a0d7c5b70b978d0c2f653d6ecb964f
Anton Butovich <abutovich@gmail.com>
2014-09-10 11:06:40 +0300

    set correct hadoop cookbook version in metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "abutovich@qubell.com"
 license          "All rights reserved"
 description      "Provision Cloudera Hadoop"
 name		 "cloudera"
-version          "0.1.0"
+version          "1.0.0"
 
 depends "yum", '~> 3.3.1'
 depends "ntp", '~> 1.6.4'
